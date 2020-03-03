76383688a26e1d46cd83ee9e6ab45621692cd7f8
Glen Cornell <glen.m.cornell@gmail.com>
2020-03-02 21:03:25 -0500

    Adding copyright notice and added support for long_float signals & properties.

--- src/aof-core-properties.ads
+++ src/aof-core-properties.ads
@@ -1,3 +1,19 @@
+--  Copyright (C) 2020 Glen Cornell <glen.m.cornell@gmail.com>
+--  
+--  This program is free software: you can redistribute it and/or
+--  modify it under the terms of the GNU General Public License as
+--  published by the Free Software Foundation, either version 3 of the
+--  License, or (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+--  General Public License for more details.
+--
+--  You should have received a copy of the GNU General Public License
+--  along with this program.  If not, see
+--  <http://www.gnu.org/licenses/>.
+
 with Aof.Core.Root_Objects;
 with Aof.Core.Generic_Properties;
 with Ada.Strings.Unbounded;
@@ -28,5 +44,9 @@ package Aof.Core.Properties is
      (Object        => Aof.Core.Root_Objects.Root_Object,
       Access_Object => Aof.Core.Root_Objects.Access_Object,
       T             => Float);
+   package Long_Floats is new Aof.Core.Generic_Properties 
+     (Object        => Aof.Core.Root_Objects.Root_Object,
+      Access_Object => Aof.Core.Root_Objects.Access_Object,
+      T             => Long_Float);
    
 end Aof.Core.Properties;
--- src/aof-core-signals.ads
+++ src/aof-core-signals.ads
@@ -1,5 +1,22 @@
+--  Copyright (C) 2020 Glen Cornell <glen.m.cornell@gmail.com>
+--  
+--  This program is free software: you can redistribute it and/or
+--  modify it under the terms of the GNU General Public License as
+--  published by the Free Software Foundation, either version 3 of the
+--  License, or (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+--  General Public License for more details.
+--
+--  You should have received a copy of the GNU General Public License
+--  along with this program.  If not, see
+--  <http://www.gnu.org/licenses/>.
+
 with Aof.Core.Root_Objects;
 with Aof.Core.Generic_Signals;
+with Ada.Strings.Unbounded;
 
 package Aof.Core.Signals is
    
@@ -13,6 +30,11 @@ package Aof.Core.Signals is
      (Object        => Aof.Core.Root_Objects.Root_Object,
       Access_Object => Aof.Core.Root_Objects.Access_Object,
       Param_1       => Aof.Core.Root_Objects.Access_Object);
+   
+   package Strings is new Aof.Core.Generic_Signals.S1
+     (Object        => Aof.Core.Root_Objects.Root_Object,
+      Access_Object => Aof.Core.Root_Objects.Access_Object,
+      Param_1       => Ada.Strings.Unbounded.Unbounded_String);
    
    package Naturals is new Aof.Core.Generic_Signals.S1
      (Object        => Aof.Core.Root_Objects.Root_Object,
@@ -28,5 +50,10 @@ package Aof.Core.Signals is
      (Object        => Aof.Core.Root_Objects.Root_Object,
       Access_Object => Aof.Core.Root_Objects.Access_Object,
       Param_1       => Float);
+   
+   package Long_Floats is new Aof.Core.Generic_Signals.S1
+     (Object        => Aof.Core.Root_Objects.Root_Object,
+      Access_Object => Aof.Core.Root_Objects.Access_Object,
+      Param_1       => Long_Float);
    
 end Aof.Core.Signals;
